ca2d2bd01ebe0717c86f7665eb4bf151ba6d673c
kingfisher09 <ofisher99@googlemail.com>
2024-12-27 13:36:03 +0000

    v0.4
    Added Slip, control curves, normal driving mode. Still built for Seeed Studio XIAO nRF52840

--- Sabrebrain.ino
+++ Sabrebrain.ino
@@ -13,8 +13,6 @@ const int headPin = 9;
 // spinChan = 4;
 // headChan = 5;
 
-int slipSig = 1500;  // need to implement this!
-
 float rotationSum = 0;
 int lightWidth = 15;
 
@@ -27,15 +25,33 @@ float angle = 0;
 int nextChan = 2;
 int spin_command = 0;  //out of 1000
 int translate = 0;     //out of 1000
+int slip = 0;           //out of 1000
 float headChange = 0;
 int cutoff = 0;
 
 const int SERVO_MIN = 1000;  // Minimum servo signal
 const int SERVO_MAX = 2000;  // Maximum servo signal
 
+int powerCurve(int x){
+   int power = 3;
+  long divisor = (pow(1000, power - 1));
+  return (pow(x, power))/divisor;
+}
+
 void read_chans() {  // cycles through the 4 channels reading updating a different one each tim it is called
 
   int readSig = pulseIn(nextChan, HIGH, 10000);
+
+  int deadzone = 30;
+  if (nextChan == 4) {  // deadzones
+    if (readSig <= 1000 + deadzone) {
+      readSig = 1000;
+    }
+  } else {
+    if (readSig >= 1500 - deadzone && readSig <= 1500 + deadzone) {
+      readSig = 1500;
+    }
+  }
 
   if (readSig == 0) {  // if a wire comes unplugged stop!
     Left_Motor.writeMicroseconds(1500);
@@ -47,10 +63,10 @@ void read_chans() {  // cycles through the 4 channels reading updating a differe
   }
 
   if (nextChan == 2) {
-    slipSig = readSig;
+    slip = powerCurve(servoTothoucentage(readSig));
     nextChan = 3;
   } else if (nextChan == 3) {
-    translate = servoTothoucentage(readSig);
+    translate = powerCurve(servoTothoucentage(readSig));
     nextChan = 4;
   } else if (nextChan == 4) {
     spin_command = map(readSig, SERVO_MIN, SERVO_MAX, 0, 1000);
@@ -68,6 +84,12 @@ float servoTothoucentage(int servoSignal) {
 
 int thoucentageToServo(float thoucentage) {
   // Map the thoucentage to the corresponding servo signal
+  if (thoucentage > 1000){  // cap to ± 1000
+    thoucentage = 1000;
+  }
+  if (thoucentage < -1000){
+    thoucentage = -1000;
+  }
   return map(thoucentage, -1000, 1000, SERVO_MIN, SERVO_MAX);
 }
 
@@ -86,6 +108,7 @@ void setup() {
   digitalWrite(headPin, HIGH);
   delay(5000);
   digitalWrite(headPin, LOW);
+  delay(500);
 
 
   // Call .begin() to configure the IMU
@@ -98,35 +121,52 @@ void setup() {
 
 void loop() {
   read_chans();
-  float looptime = static_cast<float>(millis() - before);
 
-  before = millis();  // for measuring looptime
+  if (spin_command > 0) {  // spinning mode
 
-  float zrotReading = myIMU.readFloatGyroZ() + 0.07;
-  angle = fmod(angle + (zrotReading * looptime / 1000) + 360 + headChange, 360);  // will not work if rotate more than 360° negative per loop
+    float looptime = static_cast<float>(millis() - before);
 
-  Serial.print("Looptime: ");
-  Serial.println(looptime);
-  Serial.println(angle);
+    before = millis();  // for measuring looptime
 
-  if (angle < lightWidth || angle > (360 - lightWidth)) {  // check whether to flash heading LED
+    float zrotReading = myIMU.readFloatGyroZ() + 0.07;
+    angle = fmod(angle + (zrotReading * looptime / 1000) + 360 + headChange, 360);  // will not work if rotate more than 360° negative per loop
+
+    Serial.print("Looptime: ");
+    Serial.println(looptime);
+    Serial.println(angle);
+
+    if (angle < lightWidth || angle > (360 - lightWidth)) {  // check whether to flash heading LED
+      digitalWrite(headPin, HIGH);
+    } else {
+      digitalWrite(headPin, LOW);
+    }
+
+
+    int spin = spin_command + translate > 1000 ? 1000 - translate : spin_command;  // set motor spin speed
+    float cosresult = cos(radians(angle));
+    float sinresult = sin(radians(angle));
+    float delta = (translate * (abs(cosresult) > cutoff ? cosresult : 0)) - (slip * (abs(sinresult) > cutoff ? sinresult : 0));
+
+    int left_sig = thoucentageToServo(spin + delta);
+    int right_sig = thoucentageToServo(-spin + delta);
+    Serial.print("Right motor:");
+    Serial.print(right_sig);
+    Serial.print(" Left motor: ");
+    Serial.println(left_sig);
+
+    Left_Motor.writeMicroseconds(left_sig);
+    Right_Motor.writeMicroseconds(right_sig);
+
+  } else {  // normal robot mode
     digitalWrite(headPin, HIGH);
-  } else {
-    digitalWrite(headPin, LOW);
+    int left_sig = thoucentageToServo(slip + translate);
+    int right_sig = thoucentageToServo(-slip + translate);
+    Serial.print("Right motor:");
+    Serial.print(right_sig);
+    Serial.print(" Left motor: ");
+    Serial.println(left_sig);
+
+    Left_Motor.writeMicroseconds(left_sig);
+    Right_Motor.writeMicroseconds(right_sig);
   }
-
-
-  int spin = spin_command + translate > 1000 ? 1000 - translate : spin_command;
-  float cosresult = cos(radians(angle));
-  float delta = translate * (abs(cosresult) > cutoff ? cosresult : 0);
-
-  int left_sig = thoucentageToServo(spin + delta);
-  int right_sig = thoucentageToServo(-spin + delta);
-  Serial.print("Right motor:");
-  Serial.print(right_sig);
-  Serial.print(" Left motor: ");
-  Serial.println(left_sig);
-
-  Left_Motor.writeMicroseconds(left_sig);
-  Right_Motor.writeMicroseconds(right_sig);
 }
